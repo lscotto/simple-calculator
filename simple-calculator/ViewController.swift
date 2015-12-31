4defe49d365d610868c296862bd448dae7aedd1e
lscotto <HSECT@hospicesect.local>
2015-12-31 15:47:09 -0500

    Actions Created

--- simple-calculator/ViewController.swift
+++ simple-calculator/ViewController.swift
@@ -11,14 +11,30 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    //ENUMERATIONS
+    enum op: Operation {
+        
+        case Divide = "/"
+        case Multply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        
+    }
+    
+    
     //OUTLETS
     @IBOutlet weak var outputLabel: UILabel!
     
+    
+    //PROPERTIES
     var buttonSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftValueString = ""
+    var rightValueString = ""
     
     
     
-    
+    //FUNCTIONS
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,9 +49,59 @@ class ViewController: UIViewController {
         }
     }
     
+    func playSound() {
+        
+        
+        
+    }
+    
+    
+    //ACTIONS
     @IBAction func numberPressed(btn: UIButton!) {
         
         buttonSound.play()
+        
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onMultplyPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onPeriodPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        
+        
         
     }
     
